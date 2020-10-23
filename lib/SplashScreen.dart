9244c89b6e60eae55513b59e462e1c8cf53fa445
m1ku100 <ilham.puji100@gmail.com>
2020-10-23 12:41:40 +0700

    Splash Screen

---
+++ lib/SplashScreen.dart
@@ -0,0 +1,85 @@
+import 'dart:async';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:best_flutter_ui_templates/fitness_app/fitness_app_home_screen.dart';
+
+
+class SplashScreen extends StatefulWidget {
+  @override
+  SplashScreenState createState() => new SplashScreenState();
+}
+
+class SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  var _visible = true;
+
+  AnimationController animationController;
+  Animation<double> animation;
+
+  startTime() async {
+    var _duration = new Duration(seconds: 3);
+    return new Timer(_duration, navigationPage);
+  }
+
+  void navigationPage() {
+    // Navigator.of(context).pushReplacementNamed(PAY_TM);
+    
+    Navigator.of(context).pushReplacementNamed('/home');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = new AnimationController(
+      vsync: this,
+      duration: new Duration(seconds: 2),
+    );
+    animation =
+        new CurvedAnimation(parent: animationController, curve: Curves.easeOut);
+
+    animation.addListener(() => this.setState(() {}));
+    animationController.forward();
+
+    setState(() {
+      _visible = !_visible;
+    });
+    startTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          // new Column(
+          //   mainAxisAlignment: MainAxisAlignment.end,
+          //   mainAxisSize: MainAxisSize.min,
+          //   children: <Widget>[
+          //     Padding(
+          //       padding: EdgeInsets.only(bottom: 30.0),
+          //       child: new Image.asset(
+          //         "assets/menu/bank.png",
+          //         height: 25.0,
+          //         fit: BoxFit.scaleDown,
+          //       ),
+          //     )
+          //   ],
+          // ),
+          new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Image.asset(
+                "assets/fitness_app/logo.png",
+                width: animation.value * 100,
+                height: animation.value * 100,
+                fit: BoxFit.fitHeight,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
